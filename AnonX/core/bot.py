d5bbd9d76a99ce24fe1ac4bac0756e64c57316dc
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-08 17:05:59 +0300

    Update bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -1,16 +1,16 @@
 import sys
 
 from pyrogram import Client
-from pyrogram.types import BotCommand
 
 import config
 
 from ..logging import LOGGER
 
 
+
 class AnonXBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
             "AnonXMusic",
             api_id=config.API_ID,
@@ -23,42 +23,23 @@ class AnonXBot(Client):
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        try:
-            await self.send_message(
-                config.LOG_GROUP_ID, "Bot Started"
-            )
-        except:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
-            )
-            sys.exit()
-        if config.SET_CMDS == str(True):
-            try:
-                await self.set_bot_commands(
-                    [
-                        BotCommand("ping", "Check that bot is alive or dead"),
-                        BotCommand("play", "Starts playing the requested song"),
-                        BotCommand("skip", "Moves to the next track in queue"),
-                        BotCommand("pause", "Pause the current playing song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("end", "Clear the queue and leave voice chat"),
-                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                        BotCommand("playmode", "Allows you to change the default playmode for your chat"),
-                        BotCommand("settings", "Open the settings of the music bot for your chat.")
-                        ]
-                    )
-            except:
-                pass
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
         else:
-            pass
+            self.name = get_me.first_name
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != "administrator":
             LOGGER(__name__).error(
                 "Please promote Bot as Admin in Logger Group"
             )
             sys.exit()
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        try:
+            await self.send_message(
+                config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
