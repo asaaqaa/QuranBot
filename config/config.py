e44bfd614fb65c87b4c0b71c5d5d1a6d0059eedc
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-08 15:07:17 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -61,6 +61,8 @@ CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "12"))
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 # https://www.gbmb.org/mb-to-bytes
+SET_CMDS = getenv("SET_CMDS", False)
+#اليسع الطويل 
 
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
